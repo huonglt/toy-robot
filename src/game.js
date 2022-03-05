9ccda45ddcf4bafe4a76b3264137c8262776d30c
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-05 18:39:22 +1300

    Add placeholder for place, report, move and runGame

--- src/game.js
+++ src/game.js
@@ -8,12 +8,36 @@ const game = () => {
   let y = 0;
   let f = null;
 
+  let isFirstCommandPlace = false;
+
   /**
    * Handle move command
    * Move the toy robot one unit forward in the direction it is currently facing.
    */
 
-  const move = (x, y, f) => {};
+  const move = () => {};
+
+  /**
+   * Handle place command
+   * PLACE will put the toy robot on the table in position X,Y and facing NORTH, SOUTH, EAST or WEST.
+   * The origin (0,0) can be considered to be the SOUTH WEST most corner.
+   * @param {} x
+   * @param {*} y
+   * @param {*} f
+   */
+  const place = (x, y, f) => {};
+
+  /**
+   * Handle report command
+   * Log out position and facing direction of the robot
+   */
+  const report = () => {
+    console.log(`robot position: ${x}, ${y}, ${f}`);
+  };
+
+  const runGame = () => {
+    
+  }
 };
 
 module.exports = game;
