74bd69aa1e697d9258b39b14a5ea665b29b8231d
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-05 19:18:06 +1300

    add function validatePlaceCommand to validate PLACE command separately

--- src/util.js
+++ src/util.js
@@ -11,15 +11,20 @@ const validateCommand = (command) => {
   ) {
     return true;
   }
+
+  return validatePlaceCommand(command);
+};
+
+const validatePlaceCommand = (command) => {
   /**
    * validate PLACE command with regression expression
    * valid format: PLACE X,Y,F
    */
   const regEx = /^PLACE [0-4],[0-4],(NORTH|WEST|EAST|SOUTH)$/;
-  if (regEx.test(command)) {
-    return true;
-  }
-  return false;
+  return regEx.test(command);
 };
 
-module.exports = validateCommand;
+module.exports = {
+  validateCommand,
+  validatePlaceCommand,
+};
