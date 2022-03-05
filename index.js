aab6717238f8046e2337eef42c1416f167455804
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-05 16:56:01 +1300

    use regression expression to validate command PLACE X,Y,F

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ const promptUserForCommand = () => {
 
 /**
  * Validate command enter by user
+ * valid command are: MOVE, LEFT, RIGHT, REPORT, PLACE X,Y,F
  */
 const validateCommand = (command) => {
   if (
@@ -45,7 +46,15 @@ const validateCommand = (command) => {
   ) {
     return true;
   }
-  // place
+  /**
+   * validate PLACE command with regression expression
+   * valid format: PLACE X,Y,F
+   */
+  const regEx = /^PLACE [0-4],[0-4],(NORTH|WEST|EAST|SOUTH)$/;
+  if (regEx.test(command)) {
+    return true;
+  }
+  return false;
 };
 
 /**
