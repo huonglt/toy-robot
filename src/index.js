4b209cb6b38ec8d17b46a539c3f60585fd4fc862
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-05 18:50:57 +1300

    only need to call playGame method

--- src/index.js
+++ src/index.js
@@ -1,46 +1,4 @@
-const readline = require("readline");
-const util = require("./util.js");
+const playGame = require("./game.js");
 
-/**
- * Prompt user for command to play game
- * Type exit to quit
- */
-const promptUserForCommand = () => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  /**
-   * Recursively read user command until user type exit
-   */
-  const recursiveReadCommand = () => {
-    rl.question("Command: ", (command) => {
-      if (command == "exit") {
-        return rl.close();
-      }
-      console.log(`command: ${command}`);
-
-      // call the recursive function to prompt again
-      recursiveReadCommand();
-    });
-  };
-
-  rl.on("close", () => {
-    console.log("Toy game exit");
-    process.exit(0);
-  });
-
-  recursiveReadCommand();
-};
-
-/**
- * Main function to the game
- */
-const main = () => {
-  promptUserForCommand();
-  //const x = validateCommand("MOVE");
-  //console.log(`x = ${x}`);
-};
-
-main();
+// play game now
+playGame();
