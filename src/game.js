d1914b68bd7190b6b4d23a9f3ace679b3a8266fd
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-05 18:31:58 +1300

    Initial setup of the game

---
+++ src/game.js
@@ -0,0 +1,19 @@
+const game = () => {
+  // dimension 5 x 5
+  const MAX_X = 4;
+  const MAX_Y = 4;
+
+  // initial position of robot
+  let x = 0;
+  let y = 0;
+  let f = null;
+
+  /**
+   * Handle move command
+   * Move the toy robot one unit forward in the direction it is currently facing.
+   */
+
+  const move = (x, y, f) => {};
+};
+
+module.exports = game;
