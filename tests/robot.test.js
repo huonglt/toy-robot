fb16109b1cc6622b51449e815fee1c6b5b5a4d9a
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-06 12:04:13 +1300

    Unit test to execute commands in example 1

--- tests/robot.test.js
+++ tests/robot.test.js
@@ -2,6 +2,17 @@ const { MAX_X, MOVE, LEFT, REPORT, NORTH } = require("../src/constants.js");
 const createRobot = require("../src/robot.js");
 
 describe("test robot module", () => {
+  it("commands in example 1", () => {
+    const robot = createRobot();
+    robot.executeCommand("PLACE 0,0,NORTH");
+    robot.executeCommand(MOVE);
+    robot.executeCommand(REPORT);
+
+    const { x, y, f } = robot.getCurrentPostion();
+    expect(x).toEqual(0);
+    expect(y).toEqual(1);
+    expect(f).toEqual(NORTH);
+  });
   it("commands in example 3", () => {
     const robot = createRobot();
     robot.executeCommand("PLACE 1,2,EAST");
