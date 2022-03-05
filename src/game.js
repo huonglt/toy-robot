0627f4670948237e51d154c4f17fd5c9bf79dd8b
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-05 19:18:53 +1300

    Handle requirement first command to the robot is PLACE command. Discard all commands until a valid PLACE command executed

--- src/game.js
+++ src/game.js
@@ -1,8 +1,12 @@
 const readline = require("readline");
 const createRobot = require("./robot.js");
+const { validatePlaceCommand } = require("./util.js");
 
 const playGame = () => {
   const robot = createRobot();
+
+  let firstCommandValid = false;
+
   /**
    * Prompt user for command to play game
    * Type exit to quit
@@ -18,14 +22,29 @@ const playGame = () => {
      */
     const recursiveReadCommand = () => {
       rl.question("Command: ", (command) => {
-        console.log(`command: ${command}`);
-        if (command == "exit") {
-          return rl.close();
-        } else if (command === "REPORT") {
-          robot.report();
-        }
+        /**
+         * the first command to the robot is a PLACE  command,
+         * discard all commands in the sequence until a valid PLACE command has been executed.
+         */
+        const validPlaceCommand = validatePlaceCommand(command);
 
-        // call the recursive function to prompt again
+        if (!firstCommandValid) {
+          const validPlaceCommand = validatePlaceCommand(command);
+          if (validPlaceCommand) {
+            firstCommandValid = true;
+            console.log(`==>executing command: ${command}`);
+            robot.place(1, 1, "NORTH");
+            console.log(`subsequest command after 1st PLACE valid command`);
+          } else {
+            console.log(`==>discard command ${command}`);
+          }
+        } else {
+          // subsequent command after 1st PLACE command
+          const validCommand = validCommand(command);
+          if (validCommand) {
+            // robot to execute command
+          }
+        }
         recursiveReadCommand();
       });
     };
