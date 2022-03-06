669dda7dcf3d18c1ea568711722c26e8d095b1b9
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-06 18:22:13 +1300

    handle commnand play and option

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
-const playGame = require("./game.js");
+#!/usr/bin/env node
+const playConsoleGame = require("./game.js");
+const playGameWithCommandsFromFile = require("./gameWithCommandsFromFile.js");
 
-// play game now
-playGame();
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+
+/**
+ * To play game with commands input from consolem use play
+ * To play game with commands preloaded from data.txt file, use play -o=file
+ */
+yargs(hideBin(process.argv))
+  .command(
+    "play",
+    "Play Toy-Robot game",
+    () => {},
+    (argv) => {
+      if (argv.option === "file") {
+        playGameWithCommandsFromFile();
+      } else {
+        playConsoleGame();
+      }
+    }
+  )
+  .option("option", {
+    alias: "o",
+    type: "string",
+    description:
+      "o=file to play game with commands from file.\nOtherwise will play game via console",
+  })
+  .parse();
