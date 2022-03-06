2c4c47e1ec349d922b95a2be7209b57b34b532c4
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-06 17:02:08 +1300

    module to play game with data input from a file

--- src/gameFromFile.js
+++ src/gameFromFile.js
@@ -1,25 +1,46 @@
 const path = require("path");
 const fs = require("fs");
 const readline = require("readline");
+const { once } = require("events");
 const createRobot = require("./robot.js");
 
-const playGameWithDataFromFile = () => {
+const readCommandsFromFile = async () => {
   const filename = path.resolve(__dirname, "../data/data.txt");
   const readStream = fs.createReadStream(filename);
-  const commands = [];
+  const arr = [];
+  try {
+    const rl = readline.createInterface({
+      input: readStream,
+      crlfDelay: Infinity,
+    });
 
-  const lineReader = readline.createInterface({
-    input: readStream,
-  });
+    rl.on("line", (line) => {
+      // Process the line.
+      arr.push(line);
+    });
 
-  lineReader.on("line", (line) => {
-    console.log("Line from file:", line);
-    commands.push(line);
-  });
+    await once(rl, "close");
 
-  lineReader.on("close", () => {
-    console.log(`close event. commands = ${JSON.stringify(commands)}`);
-  });
+    console.log("File processed.");
+  } catch (err) {
+    console.error(err);
+  } finally {
+    return arr;
+  }
+};
+const playGameWithDataFromFile = async () => {
+  try {
+    const commands = await readCommandsFromFile();
+    if (commands) {
+      const robot = createRobot();
+      commands.forEach((command) => {
+        robot.executeCommand(command);
+      });
+      console.log(JSON.stringify(robot.getCurrentPostion()));
+    }
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports = playGameWithDataFromFile;
